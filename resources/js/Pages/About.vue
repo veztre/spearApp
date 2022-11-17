ac290232aca20fd63446977bdae2259c60905380
veztre <vez3@yahoo.com>
2022-11-17 08:39:53 +0800

    modify abot us

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -33,9 +33,7 @@
             </div>
             <div class="py-2">
                 <h1 class="text-2xl text-red-400 "> Spear Application </h1>
-                <p> for the student organizations at Batangas State University – Alangilan Campus. This is
-                    to assist the local student organizations in handling data processes of the
-                    administrative offices with a faster and more efficient platform. </p>
+                
             </div>
         </div>
     </BreezeAuthenticatedLayout>
